dc2d8903b32111f4a7e79e246b8bb6fe03df7dbd
ShivamNishad <shivamnishad915@gmail.com>
2023-02-15 10:06:03 +0530

    Initial commit

---
+++ module2.mjs
@@ -0,0 +1,9 @@
+const a = "Shivam";
+const b = "Nishad";
+const c = "Rohit";
+const d = "Shbham";
+
+export default b;  
+export {a};  
+export {c};  
+export {d};  
---
+++ src/components/About.js
@@ -0,0 +1,92 @@
+import React from 'react'
+
+export default function About(props) {
+
+    // const [myStyle, setMyStyle] = useState({
+    //     color: 'black',
+    //     backgroundColor: 'white'
+    // }) 
+    let myStyle = {
+        color: props.mode ==='dark'?'white':'#042743',
+        backgroundColor: props.mode ==='dark'?'rgb(36 74 104)':'white', 
+    }
+    
+    return (
+        <div className="container">
+            <h1 className="my-3" style={{color: props.mode ==='dark'?'white':'#042743'}}>About Us</h1>
+            <div className="accordion" id="accordionExample">
+                <div className="accordion-item">
+                    <h2 className="accordion-header" id="headingOne">
+                    <button className="accordion-button" type="button" style={myStyle} data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                    <strong>Analyze Your text </strong>
+                    </button>
+                    </h2>
+                    <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                    <div className="accordion-body" style={myStyle}>
+                    EditText is the perfect solution for all your text formatting needs. With our easy-to-use tools, we make it easy to edit, format, and modify text with just a few clicks. We have all the features you need to make your text look its best.. Be it word count, character count or 
+
+                    </div>
+                    </div>
+                </div>
+                <div className="accordion-item">
+                    <h2 className="accordion-header" id="headingTwo">
+                    <button className="accordion-button collapsed" style={myStyle} type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                    <strong>Free to use </strong>
+                    </button>
+                    </h2>
+                    <div id="collapseTwo" className="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+                    <div className="accordion-body" style={myStyle}>
+                    Formatting: We have formatting options to help you make your text look its best. Choose from font size, font type, font color, text alignment, and more.<br/>
+                    TextSudhar is a free character counter tool that provides instant character count & word count statistics for a given text. TextSudhar reports the number of words and characters. Thus it is suitable for writing text with word/ character limit.
+                    </div>
+                    </div>
+                </div>
+                <div className="accordion-item">
+                    <h2 className="accordion-header" id="headingThree">
+                    <button className="accordion-button collapsed" style={myStyle} type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                    <strong>Browser Compatible </strong>
+                    </button>
+                    </h2>
+                    <div id="collapseThree" className="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+                    <div className="accordion-body" style={myStyle}>
+                    This word counter software works in any web browsers such as Chrome, Firefox, Internet Explorer, Safari, Opera. It suits to count characters in facebook, blog, books, excel document, pdf document, essays, etc.
+
+                    </div>
+                    </div>
+                </div>
+
+                <div className="accordion-item">
+                    <h2 className="accordion-header" id="headingThree">
+                    <button className="accordion-button collapsed" style={myStyle} type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                    <strong>Our features include: </strong>
+                    </button>
+                    </h2>
+                    <div id="collapseThree" className="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+                    <div className="accordion-body" style={myStyle}>
+                    • Uppercase Convert: Easily convert your text from lowercase to uppercase with just one click.<br/>
+
+                    • Lowercase Convert: Quickly convert your text from uppercase to lowercase.<br/>
+
+                    • Remove Extra Spaces: Get rid of those pesky extra spaces quickly and easily.<br/>
+
+                    • Copy Text: Copy and paste text with ease.<br/>
+
+                    • Download Text: Download your text in a variety of file formats.<br/>
+
+                    • Speak Text: Hear your text read aloud.<br/>
+
+                    • Convert to Title Case: Automatically convert your text to title case.<br/>
+
+                    With Edit Text, you can quickly and easily format and modify text in a matter of seconds. Try
+
+
+
+                    </div>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+    )
+}
